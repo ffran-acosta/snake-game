195ceb6347f856bacf4ceaa18c4e2eac583b9434
ffran.acosta <francoaugustoacosta@gmail.com>
2023-02-22 11:58:18 -0300

    db migration init

---
+++ source/data/create-database.sql
@@ -0,0 +1,8 @@
+CREATE TABLE snake_score (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255),
+    level VARCHAR(255),
+    score INT
+)
+
+INSERT INTO products (name, level, score) VALUES ('Fran', 'product.jpg', 10)
