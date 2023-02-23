cf345678061c71d790b9bcf6dee724a8dbd7e83f
ffran.acosta <francoaugustoacosta@gmail.com>
2023-02-23 08:22:11 -0300

    test

--- source/data/dbRailWay.js
+++ source/data/dbRailWay.js
@@ -1,11 +1,11 @@
 const Pool = require('pg').Pool
 
 const pool = new Pool({
-    user: process.env.USER,
-    host: process.env.HOST,
-    database: process.env.DATABASE,
-    password: process.env.PASSWORD,
-    port: process.env.PORT,
+    user: process.env.S_USER,
+    host: process.env.S_HOST,
+    database: process.env.S_DATABASE,
+    password: process.env.S_PASSWORD,
+    port: process.env.S_PORT,
 })
 
 module.exports = pool
